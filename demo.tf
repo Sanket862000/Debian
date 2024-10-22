fa6ab0eec66162f85c8d606f9ea2ebd649494b3d
Sanket862000 <106148651+Sanket862000@users.noreply.github.com>
2024-10-22 09:41:29 +0530

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -1,7 +1,17 @@
 provider "aws" {
   region = "ap-south-1"
 }
-
 resource "aws_vpc" "main" {
-  cidr_block ="10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "main-vpc"
+  }
+}
+resource "aws_subnet" "main" {
+  vpc_id     = (link unavailable)
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+  tags = {
+    Name = "main-subnet"
+  }
 }
